b64b58de02576aa82285bd15d531f7ebd86f1c60
carterfm <carterf.morfitt@gmail.com>
2022-02-09 12:05:55 -0800

    Set up first model

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const exphbs = require("express-handlebars");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Express middleware for processing post and put requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Setting up the public directory to handle static requests
+app.use(express.static("public"))
+
+//Setting up handlebars
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//Allowing for the creation of session objects (currently set to last for two hours)
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        maxAge:2*60*60*1000
+     },
+     store: new SequelizeStore({
+        db: sequelize,
+      }),
+  }))
+
+// app.use('/', routes);
+
+//Syncing sequelize before setting the server to listen
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+    });
+});
+
+
