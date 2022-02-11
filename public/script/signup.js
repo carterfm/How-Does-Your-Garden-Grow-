f40d5a5d2fba43009474e4081a1e194e5614f634
Buttercup <andyandnile@gmail.com>
2022-02-10 23:22:02 -0800

    can now create a user

---
+++ public/script/signup.js
@@ -0,0 +1,33 @@
+console.log('-----------------im signup-------------------')
+// const passwordVal = document.getElementById('passwordVal').value.trim();
+
+
+
+const createUser = async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('usernameInput').value.trim();
+    const email = document.getElementById('emailInput').value.trim();
+    const password = document.getElementById('passwordInput').value.trim();
+    console.log('into the fxn')
+    // if (password!==passwordVal){
+    //   alert('Your passwords don\'t match!\nTry again!')
+    //   return
+    // }
+    // else {
+      if (username && email && password) {
+        const response = await fetch('/api/user', {
+          method: 'POST',
+          body: JSON.stringify({ username, email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert(response.statusText);
+        }
+      }
+    // }
+};
+
+document.getElementById('create').addEventListener('submit', createUser);
