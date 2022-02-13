e5e62aa987f92a7dba1d919ed09b7dc738231902
Buttercup <andyandnile@gmail.com>
2022-02-13 01:07:30 -0800

    functionality added to the drop down menu

--- public/script/main.js
+++ public/script/main.js
@@ -1,53 +1,6 @@
-const createBtn = document.getElementById('create');
-const savedGardBtn = document.getElementById('old-gard');
-const profBtn = document.getElementById('view-prof');
 
-const getCreateForm = async e => {
-    e.preventDefault();
-    const response = await fetch('/dashboard/gardens/create', {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-    })
-        if (response.ok) {
-        document.location.replace('/dashboard/gardens/create');
-        } else {
-        alert(response.statusText);
-        }
-}
 
-const getGardens = async e => {
-    e.preventDefault();
-    const response = await fetch('/dashboard/gardens', {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-    })
-        if (response.ok) {
-        document.location.replace('/dashboard/gardens');
-        } else {
-        alert(response.statusText);
-        }
-}
-
-const getProfile = async e => {
-    e.preventDefault();
-    const response = await fetch('/dashboard/profile', {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-    })
-        if (response.ok) {
-        document.location.replace('/dashboard/profile');
-        } else {
-        alert(response.statusText);
-        }
-}
-
-const logout = async (e) => {
+document.getElementById('logout').addEventListener('click', async e=>{
     e.preventDefault();
     const response = await fetch('/api/user/logout', {
       method: 'POST',
@@ -59,9 +12,4 @@ const logout = async (e) => {
     } else {
       alert(response.statusText);
     }
-  };
-
-profBtn.addEventListener('click', getProfile);
-savedGardBtn.addEventListener('click', getGardens);
-createBtn.addEventListener('click', getCreateForm);
-document.getElementById('logout').addEventListener('click', logout);
+});
